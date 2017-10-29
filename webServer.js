642c2eb5a22fb98989be07d75a8d25b325f470b5
Kevin Khieu <kkhieu@stanford.edu>
2017-10-28 19:33:07 -0700

    fixed server?

--- webServer.js
+++ webServer.js
@@ -263,7 +263,15 @@ app.get('/gender/cumulative', function(request, response) {
 	response.status(200).end(gender_cumulative);
 });
 
-var server = app.listen(3000, function () {
-    var port = server.address().port;
-    console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
+
+// COMMENT THIS OUT TO RUN LOCALLY
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+// UNCOMMENT THIS TO RUN LOCALLY
+// var server = app.listen(3000, function () {
+//     var port = server.address().port;
+//     console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
+// });
