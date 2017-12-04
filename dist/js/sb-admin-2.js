793d651d997889f6f86f7370b99829c20b3a4379
Angelina Hue <angelinahue@DN0a1feeb4.SUNet>
2017-12-03 20:17:29 -0800

    no changes

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -28,9 +28,19 @@ function UploadCSV() {
     //request.setRequestHeader('Content-type', 'multipart/form-data'); //----(*)
     request.send(formData);
 }
-
+var file = null;
 function uploadedFileChange() {
     var csv=document.getElementById('csvUploader').files[0];
+    function readFile() {
+        var reader = new FileReader();
+        reader.onload = function () {
+            console.log(reader.result);
+        };
+        // start reading the file. When it is done, calls the onload event defined above.
+        reader.readAsBinaryString(csv);
+    }
+   // readFile();
+   file = csv;
     var status = document.getElementById('status');
     status.innerHTML = csv.name
 }
@@ -97,6 +107,8 @@ function showGraphOptions(){
     document.getElementById("calendar").style.display = "initial";
     document.getElementById("linegraph").style.display = "initial";
     document.getElementById("Launchpage").style.display="none";
+    document.getElementById("calendarimage").style.display = "initial";
+    document.getElementById("linegraphimage").style.display = "initial";
 }
 
 function goBackToLaunch(){
@@ -108,10 +120,12 @@ function backBtnClick() {
     //function() {
         //document.getElementById("calendar").style.display = "none";
         // showGraphOptions();
-        document.getElementById("graphOptions").style.display="initial";
+    document.getElementById("graphOptions").style.display="initial";
     document.getElementById("Launchpage").style.display="none";
     document.getElementById("calendar").style.display = "initial";
     document.getElementById("linegraph").style.display = "initial";
+    document.getElementById("calendarimage").style.display = "initial";
+    document.getElementById("linegraphimage").style.display = "initial";
    // }
 }
 
@@ -215,83 +229,83 @@ function displayGraph(id){
         document.getElementById("calendar").style.display = "none";
         document.getElementById("linegraph").style.display = "initial";
         document.getElementById("linegraphimage").style.display = "none";
-        var svg = d3.select("svg"),
-            margin = {top: 20, right: 80, bottom: 30, left: 50},
-            width = svg.attr("width") - margin.left - margin.right,
-            height = svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // var svg = d3.select("svg"),
+        //     margin = {top: 20, right: 80, bottom: 30, left: 50},
+        //     width = svg.attr("width") - margin.left - margin.right,
+        //     height = svg.attr("height") - margin.top - margin.bottom,
+        //     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var parseTime = d3.timeParse("%Y%m%d");
+        // var parseTime = d3.timeParse("%Y%m%d");
 
-        var x = d3.scaleTime().range([0, width]),
-            y = d3.scaleLinear().range([height, 0]),
-            z = d3.scaleOrdinal(d3.schemeCategory10);
+        // var x = d3.scaleTime().range([0, width]),
+        //     y = d3.scaleLinear().range([height, 0]),
+        //     z = d3.scaleOrdinal(d3.schemeCategory10);
 
-        var line = d3.line()
-            .curve(d3.curveBasis)
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.temperature); });
+        // var line = d3.line()
+        //     .curve(d3.curveBasis)
+        //     .x(function(d) { return x(d.date); })
+        //     .y(function(d) { return y(d.temperature); });
 
-        d3.csv("dji.csv", type, function(error, data) {
-          if (error) throw error;
+        // d3.csv("dji.csv", type, function(error, data) {
+        //   if (error) throw error;
 
-          var cities = data.columns.slice(1).map(function(id) {
-            return {
-              id: id,
-              values: data.map(function(d) {
-                return {date: d.date, temperature: d[id]};
-              })
-            };
-          });
+        //   var cities = data.columns.slice(1).map(function(id) {
+        //     return {
+        //       id: id,
+        //       values: data.map(function(d) {
+        //         return {date: d.date, temperature: d[id]};
+        //       })
+        //     };
+        //   });
 
-          x.domain(d3.extent(data, function(d) { return d.date; }));
+        //   x.domain(d3.extent(data, function(d) { return d.date; }));
 
-          y.domain([
-            d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
-            d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
-          ]);
+        //   y.domain([
+        //     d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
+        //     d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
+        //   ]);
 
-          z.domain(cities.map(function(c) { return c.id; }));
+        //   z.domain(cities.map(function(c) { return c.id; }));
 
-          g.append("g")
-              .attr("class", "axis axis--x")
-              .attr("transform", "translate(0," + height + ")")
-              .call(d3.axisBottom(x));
+        //   g.append("g")
+        //       .attr("class", "axis axis--x")
+        //       .attr("transform", "translate(0," + height + ")")
+        //       .call(d3.axisBottom(x));
 
-          g.append("g")
-              .attr("class", "axis axis--y")
-              .call(d3.axisLeft(y))
-            .append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("y", 6)
-              .attr("dy", "0.71em")
-              .attr("fill", "#000")
-              .text("Temperature, ºF");
+        //   g.append("g")
+        //       .attr("class", "axis axis--y")
+        //       .call(d3.axisLeft(y))
+        //     .append("text")
+        //       .attr("transform", "rotate(-90)")
+        //       .attr("y", 6)
+        //       .attr("dy", "0.71em")
+        //       .attr("fill", "#000")
+        //       .text("Temperature, ºF");
 
-          var city = g.selectAll(".city")
-            .data(cities)
-            .enter().append("g")
-              .attr("class", "city");
+        //   var city = g.selectAll(".city")
+        //     .data(cities)
+        //     .enter().append("g")
+        //       .attr("class", "city");
 
-          city.append("path")
-              .attr("class", "line")
-              .attr("d", function(d) { return line(d.values); })
-              .style("stroke", function(d) { return z(d.id); });
+        //   city.append("path")
+        //       .attr("class", "line")
+        //       .attr("d", function(d) { return line(d.values); })
+        //       .style("stroke", function(d) { return z(d.id); });
 
-          city.append("text")
-              .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-              .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-              .attr("x", 3)
-              .attr("dy", "0.35em")
-              .style("font", "10px sans-serif")
-              .text(function(d) { return d.id; });
-        });
+        //   city.append("text")
+        //       .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+        //       .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+        //       .attr("x", 3)
+        //       .attr("dy", "0.35em")
+        //       .style("font", "10px sans-serif")
+        //       .text(function(d) { return d.id; });
+        // });
 
-        function type(d, _, columns) {
-          d.date = parseTime(d.date);
-          for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
-          return d;
-        }
+        // function type(d, _, columns) {
+        //   d.date = parseTime(d.date);
+        //   for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
+        //   return d;
+        // }
     }
 }
 
